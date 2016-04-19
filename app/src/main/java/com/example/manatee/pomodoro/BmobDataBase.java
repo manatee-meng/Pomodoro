0b6b954673527fab89e4f0fc7386d1db46ca5abd
manatee <manatee@mengzilu.com>
2016-04-20 00:39:19 +0800

    1、上传云端完成或失败后能弹出toast 2、正在做从云端获取数据恢复本地数据，现在需要在BmobDataBase类中写数据到数据库。我期望能在PO对象中完成这个操作。这需要DBhelper是单例模式，并在MainActivity中创建单例。问题是如何在lazy方式下获取context。也许我对PO对象的理解还不到位，说不定有更优雅的方式处理。

--- app/src/main/java/com/example/manatee/pomodoro/BmobDataBase.java
+++ app/src/main/java/com/example/manatee/pomodoro/BmobDataBase.java
@@ -5,14 +5,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
 import android.os.Message;
-import android.os.SystemClock;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import cn.bmob.v3.BmobObject;
 import cn.bmob.v3.BmobQuery;
@@ -29,15 +26,19 @@ import cn.bmob.v3.listener.UpdateListener;
 public class BmobDataBase {
     private MyDataBaseHelper dbHelper;
     private Context mContext;
-    private List<BmobObject> remoteInsertList = new ArrayList<BmobObject>();
-    private List<BmobObject> remoteUpdateList = new ArrayList<BmobObject>();
-    private List<BmobObject> remoteDeleteList = new ArrayList<BmobObject>();
-    private List<ScheduleUnit> remoteList = new ArrayList<ScheduleUnit>();
-    private List<ScheduleUnit> localList = new ArrayList<ScheduleUnit>();
-    private boolean InsertFinish;
-    private boolean UpdateFinish;
-    private boolean DeleteFinish;
-    private boolean alreadyFailed;                              //为了只让同步失败显示一次
+    private List<BmobObject> remoteInsertList = new ArrayList<>();
+    private List<BmobObject> remoteUpdateList = new ArrayList<>();
+    private List<BmobObject> remoteDeleteList = new ArrayList<>();
+    private List<ScheduleUnit> remoteList = new ArrayList<>();
+    private List<ScheduleUnit> localList = new ArrayList<>();
+    private List<BmobObject> localInsertList = new ArrayList<>();
+    private int remoteInsertNum;                                      //一次同步需要操作的数据个数
+    private int remoteUpdateNum;
+    private int remoteDeleteNum;
+    private int localInsertNum;
+    private boolean uploadFailed;                               //为了只让失败显示一次
+    private boolean downloadFailed;
+    private boolean isDownload;                                 //true代表从download，false代表upload
     private static final int ReadRemoteInit = 0;
     private static final int ReadRemoteFinish = 1;
     private static final int ReadRemoteError = 2;
@@ -48,7 +49,8 @@ public class BmobDataBase {
         mContext = context;
         dbHelper = new MyDataBaseHelper(context, "Pomodoro.db", null, 1);
         dbHelper.getWritableDatabase();
-        alreadyFailed = false;
+        uploadFailed = false;
+        downloadFailed = false;
     }
 
     private void getDataFromBmob() {
@@ -85,24 +87,6 @@ public class BmobDataBase {
         }
     }
 
-    public void upload() {
-        //清零所有list，重新查询
-        remoteInsertList.clear();
-        remoteUpdateList.clear();
-        remoteList.clear();
-        InsertFinish = false;
-        UpdateFinish = false;
-        DeleteFinish = false;
-        //查询本地数据和云端数据
-        getDataFormLocal();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                getDataFromBmob();
-            }
-        }).start();
-    }
-
     private ScheduleUnit findUnitInListById(List<ScheduleUnit> list, Long id) {
         ScheduleUnit unitBmob;
         for (Iterator<ScheduleUnit> it = list.iterator(); it.hasNext(); ) {
@@ -113,7 +97,27 @@ public class BmobDataBase {
         return null;
     }
 
-    private void UpLoadSync() {
+    public void upload() {
+        isDownload = false;
+        //清零所有list，重新查询
+        remoteInsertList.clear();
+        remoteUpdateList.clear();
+        remoteList.clear();
+        remoteInsertNum = 0;
+        remoteUpdateNum = 0;
+        remoteDeleteNum = 0;
+        uploadFailed = false;
+        //查询本地数据和云端数据
+        getDataFormLocal();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getDataFromBmob();
+            }
+        }).start();
+    }
+
+    private void uploadSync() {
         //根据本地和云端数据的不同，计算update、delete、insert的list
         for (Iterator<ScheduleUnit> it = localList.iterator(); it.hasNext(); ) {
             ScheduleUnit unitLocal = it.next();
@@ -123,57 +127,87 @@ public class BmobDataBase {
             } else {
                 ScheduleUnitPO localPO = new ScheduleUnitPO(unitLocal);
                 ScheduleUnitPO remotePO = new ScheduleUnitPO(unitRemote);
-                boolean flag = localPO.isSame(remotePO);
-                if (flag == false) {                                            //云端有，但数据和本地不同，需要update
+                if (!localPO.isSame(remotePO)) {                          //云端有，但数据和本地不同，需要update
                     ScheduleUnit unitTmp = localPO.toBmob();
                     unitTmp.setObjectId(unitRemote.getObjectId());
                     remoteUpdateList.add(unitTmp);
                 }
                 remoteList.remove(unitRemote);                            //云端和本地都有的数据移除，剩下的就是云端需要删除的
             }
-            if (remoteInsertList.size() == 50) {                              //满50个就要上传一次
-                insertRemote();
-            }
-            if (remoteUpdateList.size() == 50) {
-                updateRemote();
-            }
         }
         if (remoteInsertList.size() != 0) {                                   //最后再把剩下的上传
+            remoteInsertNum = remoteInsertList.size();
             insertRemote();
         }
         if (remoteUpdateList.size() != 0) {
+            remoteUpdateNum = remoteUpdateList.size();
             updateRemote();
         }
 
         for (Iterator<ScheduleUnit> it = remoteList.iterator(); it.hasNext(); ) {  //云端列表剩下的就是需要删除的了
             remoteDeleteList.add(it.next());
-            if (remoteDeleteList.size() == 50) {
-                deleteRemote();
-            }
         }
         if (remoteDeleteList.size() != 0) {
+            remoteDeleteNum = remoteDeleteList.size();
             deleteRemote();
         }
+        if (remoteDeleteNum == 0 && remoteUpdateNum == 0 && remoteInsertNum == 0)
+            Toast.makeText(mContext, "已与云端同步", Toast.LENGTH_SHORT).show();
+    }
+
+    public void download() {
+        isDownload = true;
+        localInsertNum = 0;
+        localInsertList.clear();
+        downloadFailed = false;
+        //查询本地数据和云端数据
+        getDataFormLocal();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getDataFromBmob();
+            }
+        }).start();
+    }
+
+    private void downloadSync() {
+        //只要本地存在数据，则以本地为准
+        //只从云端获取本地没有的数据
+        for (Iterator<ScheduleUnit> it = remoteList.iterator(); it.hasNext(); ) {
+            ScheduleUnit unitRemote = it.next();
+            ScheduleUnit unitLocal = findUnitInListById(localList, unitRemote.getUnitId());
+            if (unitRemote == null) {                                         //本地没有，需要insert
+                localInsertList.add(unitLocal);
+            }
+        }
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        while (localInsertList.size() != 0){
+
+        }
+
     }
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case ReadRemoteFinish:
-                    UpLoadSync();
+                    if (isDownload)
+                        downloadSync();
+                    else
+                        uploadSync();
                     break;
                 case ReadRemoteError:
                     Toast.makeText(mContext, "无法从云端获取数据", Toast.LENGTH_SHORT).show();   //只会失败一次，不需要alreadyFailed
                     break;
                 case UpLoadRemoteError:
-                    if (!alreadyFailed){                                                    //增删改都要报这个错误，所以需要置位
+                    if (!uploadFailed) {                                                    //增删改都要报这个错误，所以需要置位
                         Toast.makeText(mContext, "上传云端出错", Toast.LENGTH_SHORT).show();//alreadyFailed让Toast只显示一次
-                        alreadyFailed = true;
+                        uploadFailed = true;
                     }
                     break;
                 case UpLoadRemoteFinish:
-                    if ( InsertFinish && UpdateFinish && DeleteFinish )
-                        Toast.makeText(mContext, "已同步到云端", Toast.LENGTH_SHORT).show();
+                    if (remoteDeleteNum <= 0 && remoteUpdateNum <= 0 && remoteInsertNum <= 0)
+                        Toast.makeText(mContext, "已与云端同步", Toast.LENGTH_SHORT).show();
                     break;
                 default:
                     break;
@@ -182,22 +216,22 @@ public class BmobDataBase {
     };
 
     private void insertRemote() {
-        while (remoteInsertList.size() != 0){
+        while (remoteInsertList.size() != 0) {
             List<BmobObject> tmpList = new ArrayList<BmobObject>();
             int len;
-            if (remoteInsertList.size() >= 50){
+            if (remoteInsertList.size() >= 50) {
                 len = 50;
-            }else {
+            } else {
                 len = remoteInsertList.size();
             }
-            for(int i = 0; i < len; i++){
+            for (int i = 0; i < len; i++) {
                 tmpList.add(remoteInsertList.get(0));
                 remoteInsertList.remove(0);
             }
             new BmobObject().insertBatch(mContext, tmpList, new SaveListener() {
                 @Override
                 public void onSuccess() {
-                    InsertFinish = true;
+                    remoteInsertNum -= 50;
                     Message message = new Message();
                     message.what = UpLoadRemoteFinish;
                     handler.sendMessage(message);
@@ -214,22 +248,22 @@ public class BmobDataBase {
     }
 
     private void updateRemote() {
-        while (remoteUpdateList.size() != 0){
+        while (remoteUpdateList.size() != 0) {
             List<BmobObject> tmpList = new ArrayList<BmobObject>();
             int len;
-            if (remoteUpdateList.size() >= 50){
+            if (remoteUpdateList.size() >= 50) {
                 len = 50;
-            }else {
+            } else {
                 len = remoteUpdateList.size();
             }
-            for(int i = 0; i < len; i++){
+            for (int i = 0; i < len; i++) {
                 tmpList.add(remoteUpdateList.get(0));
                 remoteUpdateList.remove(0);
             }
             new BmobObject().updateBatch(mContext, tmpList, new UpdateListener() {
                 @Override
                 public void onSuccess() {
-                    UpdateFinish = true;
+                    remoteUpdateNum -= 50;
                     Message message = new Message();
                     message.what = UpLoadRemoteFinish;
                     handler.sendMessage(message);
@@ -246,22 +280,22 @@ public class BmobDataBase {
     }
 
     private void deleteRemote() {
-        while (remoteDeleteList.size() != 0){
+        while (remoteDeleteList.size() != 0) {
             List<BmobObject> tmpList = new ArrayList<BmobObject>();
             int len;
-            if (remoteDeleteList.size() >= 50){
+            if (remoteDeleteList.size() >= 50) {
                 len = 50;
-            }else {
+            } else {
                 len = remoteDeleteList.size();
             }
-            for(int i = 0; i < len; i++){
+            for (int i = 0; i < len; i++) {
                 tmpList.add(remoteDeleteList.get(0));
                 remoteDeleteList.remove(0);
             }
             new BmobObject().deleteBatch(mContext, tmpList, new DeleteListener() {
                 @Override
                 public void onSuccess() {
-                    DeleteFinish = true;
+                    remoteDeleteNum -= 50;
                     Message message = new Message();
                     message.what = UpLoadRemoteFinish;
                     handler.sendMessage(message);
